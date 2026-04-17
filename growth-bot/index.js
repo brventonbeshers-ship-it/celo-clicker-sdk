a78ee4628cee9066159f60a36d8369b968a43367
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-17 13:18:36 +0300

    chore: clean up index.js

--- growth-bot/index.js
+++
@@ -1,5 +0,0 @@
-import * as sdk from 'celo-clicker-sdk';
-
-console.log('Growth bot running for celo-clicker-sdk...');
-console.log('SDK exports:', Object.keys(sdk).join(', '));
-console.log('Smoke check passed.');
